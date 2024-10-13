66803a835941d37f30474c67eac0d8b234b654c2
siwoo <jamieyun06@gmail.com>
2024-10-13 13:39:50 +0900

    separate js file

---
+++ assets/js/heaterfunc.js
@@ -0,0 +1,33 @@
+let currentTemp = 30; // Define the initial temperature
+
+function increaseTemp() {
+    currentTemp++; // Increment the temperature
+    document.getElementsByClassName("temp")[0].innerHTML = currentTemp;
+}
+
+function decreaseTemp() {
+    if (currentTemp > 0) { // Check if temperature is above 0
+        currentTemp--;
+    }
+    document.getElementsByClassName("temp")[0].innerHTML = currentTemp;
+}
+
+function on() {
+    var audio = document.getElementById("audio");
+    audio.play();
+
+    var onbutton = document.getElementById(".brown-noise2");
+    onbutton.style.opacity = "20%";
+    var offbutton = document.getElementById(".brown-noise3");
+    offbutton.style.opacity = "100%";
+}
+
+function off() {
+    var audio = document.getElementById("audio");
+    audio.pause();
+
+    var offbutton = document.getElementById(".brown-noise3");
+    offbutton.style.opacity = "20%";
+    var onbutton = document.getElementById(".brown-noise2");
+    onbutton.style.opacity = "100%";
+}
